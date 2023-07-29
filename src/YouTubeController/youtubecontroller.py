4ef091edfcc8b3679986df1cd6d4e7f363d227d8
MKassab215 <mahmoud.kassab@steamhub.info>
2023-07-29 11:49:35 +0300

    x

--- src/YouTubeController/youtubecontroller.py
+++ src/YouTubeController/youtubecontroller.py
@@ -10,7 +10,7 @@ import subprocess
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.chrome.service import Service
 
 
 
@@ -49,8 +49,10 @@ class YoutubeController:
                 self.__create_new_instance()
             chrome_options = Options()
             chrome_options.add_argument('--log-level=3')
-            chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-            driver = webdriver.Chrome(ChromeDriverManager(print_first_line=False).install(),options=chrome_options,)
+            # chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+
+            # driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options,)
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
         print(f'\033[96mYou are all set!\033[0m\n')
         return driver
     def __clear(self):
